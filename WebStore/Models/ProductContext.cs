c8e834569fe54d9a1f824b975e1d8ddd29f65814
MonteC73 <jarekucinski@gmail.com>
2016-12-20 13:36:34 +0100

    CreatedDataAccessLayer

---
+++ WebStore/Models/ProductContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace WebStore.Models
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext() : base("WebStore")
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
